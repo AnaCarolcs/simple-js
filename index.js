106777484cb56e98da9c3c5cbddf336cf4f47249
Ana Carolina Carvalho <ancarolcs@gmail.com>
2021-01-27 16:06:27 -0300

    [WIP] Parte 02

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Conta } from "./Conta.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Ana", 11122233309);
 const cliente2 = new Cliente("Carol", 88822233309);
 
-const conta1 = new ContaCorrente(cliente1, 1001);
-const conta2 = new ContaCorrente(cliente2, 1002);
+const conta1 = new ContaCorrente(cliente2, 1002);
+const conta2 = new ContaPoupanca(50, cliente1, 1001);
+const conta3 = new ContaSalario(cliente1, 1001)
 
-//console.log(conta1);
-conta1.depositar(500);
-//console.log(conta2);
-
-let valor = 200;
-conta1.transferir(valor, conta2);
 
 console.log(conta1);
-console.log(ContaCorrente.numeroDeContas);
+console.log(conta2);
+console.log(conta3);
